7404cc2fc7509fa4712fcf880cf42503c3e2daa8
Íñigo Aréjula Aísa <arejula27@gmail.com>
2020-03-30 20:22:37 +0200

    ej 6

---
+++ ej6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then 
+	echo "Uso: ej6.sh prefijo dir"
+	exit 85
+fi
+
+files=$(find $2 -mtime +10)
+dat=$(date +"%y-%m-%d")
+tar -cvf  ${1}_${dat}.tar $files
+
+
+
+
